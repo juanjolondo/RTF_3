cae32c5a5d9d03fd62fb39e62ab68c5938acef57
Juan José Londoño T <33065960+juanjolondo@users.noreply.github.com>
2017-10-27 17:48:39 -0500

    Creación de la capa DAO
    Paquete: DAO
    Dentro del paquete DAO se crearon las clases que se conectan a archivos y sirven como base de datos de la aplicación.
    
    A la clase Usuario del paquete entidades se le agrego un atributo nuevo para que funcione en conjunto con las clases del DAO

--- src/co/analisis2/rtf3/entidades/Usuario.java
+++ src/co/analisis2/rtf3/entidades/Usuario.java
@@ -4,14 +4,24 @@ public class Usuario {
 	private String idUsuario;
 	private String usuario;
 	private String clave;
+	private String tipo;
 	
 	public Usuario(){
 	}
 	
-	public Usuario(String idUsuario, String usuario, String clave) {
+	public Usuario(String idUsuario, String usuario, String clave, String tipo) {
 		this.idUsuario = idUsuario;
 		this.usuario = usuario;
 		this.clave = clave;
+		this.tipo = tipo;
+	}
+	
+	public String getTipo(){
+		return tipo;
+	}
+	
+	public void setTipo(String tipo){
+		this.tipo = tipo;
 	}
 
 	public String getIdUsuario() {
